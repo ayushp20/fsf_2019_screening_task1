000632208ec1e33377bc32cf1ecd659811a509b6
ayushp20 <ayush.p.20@gmail.com>
2019-03-11 18:42:27 +0000

    created the detail view for each task

--- taskmanager/home/urls.py
+++ taskmanager/home/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
-from .views import PostListView
+from .views import TaskListView, TaskDetailView
 
 urlpatterns = [
-    path('', PostListView.as_view(), name =  'app-home'),
+    path('', TaskListView.as_view(), name =  'app-home'),
+    path('task/<int:pk>/', TaskDetailView.as_view(), name =  'task-detail'),
     path('about/', views.about, name = 'app-about'),
 ]
--- taskmanager/home/views.py
+++ taskmanager/home/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Task
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 def home (request):
 	context = {
@@ -9,11 +9,17 @@ def home (request):
 	return render (request, 'home/home.html', context)
 
 
-class PostListView(ListView):
+class TaskListView(ListView):
 	model = Task
 	template_name = 'home/home.html'
 	context_object_name = 'tasks'
 	ordering = ['-date_posted']
+
+
+class TaskDetailView(DetailView):
+	model = Task
+
+
 
 def about(request):
 	return render (request, 'home/about.html', {'title':'About'})
