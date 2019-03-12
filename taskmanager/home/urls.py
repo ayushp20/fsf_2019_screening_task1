ca0b7ea9f4190d0a4998f21cf877a673b5ff29bb
ayushp20 <ayush.p.20@gmail.com>
2019-03-12 14:54:17 +0000

    Added delete task functionality

--- taskmanager/home/urls.py
+++ taskmanager/home/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from . import views
-from .views import TaskListView, TaskDetailView, TaskCreateView, TaskUpdateView
+from .views import TaskListView, TaskDetailView, TaskCreateView, TaskUpdateView, TaskDeleteView
 
 urlpatterns = [
     path('', TaskListView.as_view(), name =  'app-home'),
     path('task/<int:pk>/', TaskDetailView.as_view(), name =  'task-detail'),
     path('task/new/', TaskCreateView.as_view(), name =  'task-create'),
-    path('task/<int:pk>/', TaskUpdateView.as_view(), name =  'task-update'),
+    path('task/<int:pk>/update', TaskUpdateView.as_view(), name =  'task-update'),
+    path('task/<int:pk>/delete', TaskDeleteView.as_view(), name =  'task-delete'),
+    
     path('about/', views.about, name = 'app-about'),
 
-]
+ ]
--- taskmanager/home/views.py
+++ taskmanager/home/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Task
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 def home (request):
 	context = {
 		'tasks': Task.objects.all()
@@ -28,13 +28,29 @@ class TaskCreateView( LoginRequiredMixin, CreateView):
 		form.instance.creator = self.request.user
 		return super().form_valid(form)
 
-class TaskUpdateView( LoginRequiredMixin, UpdateView):
+class TaskUpdateView( LoginRequiredMixin, UserPassesTestMixin, UpdateView):
 	model = Task
 	fields = ['title', 'content']
 
 	def form_valid(self, form):
 		form.instance.creator = self.request.user
 		return super().form_valid(form)
+
+	def test_func(self):
+		task = self.get_object()
+		if self.request.user == task.creator:
+			return True
+		return False
+
+class TaskDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+	model = Task
+	success_url = '/'
+
+	def test_func(self):
+		task = self.get_object()
+		if self.request.user == task.creator:
+			return True
+		return False
 
 def about(request):
 	return render (request, 'home/about.html', {'title':'About'})
